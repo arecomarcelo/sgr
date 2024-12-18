ad42df499aaad2a86c682b3383a579d5f19681f5
arecomarcelo <marcelo.areco@hotmail.com>
2024-12-18 20:08:55 -0300

    Ajuste código

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /media/areco/Backup/Oficial/Linux/Projetos/sgr/venv
+set -gx VIRTUAL_ENV /media/areco/Backup/Linux/sgr/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
